6cc1c47994ee649f77f7662d938d68774520b363
Milton Orellana <go0211032020@unab.edu.sv>
2021-02-26 18:46:43 -0600

    formulario

--- Laboratorio1.Pogra1/Laboratorio1.Pogra1/VISTA/Form1.Designer.cs
+++ Laboratorio1.Pogra1/Laboratorio1.Pogra1/VISTA/Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace Laboratorio1.Pogra1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 100);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "USUARIO";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(462, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 100);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ADMINISTRADOR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
